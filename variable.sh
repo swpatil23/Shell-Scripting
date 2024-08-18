abe5777ff7e829f7b1ee8017bd484f41e9d08537
Ubuntu <ubuntu@ip-172-31-15-155.ap-south-1.compute.internal>
2024-08-18 13:11:18 +0000

    pushing to github

---
+++ variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This is a shell script for declaring a variable
+
+a=9
+name="Shweta"
+age=25
+
+echo "My name is $name and my age is $age"
+
+
+# Within the script we can change the values of the varible.
+
+name="Riyan"
+
+echo "My name is $name"
+
+
