a35fb751d41fd6600649f73d5da74f04cfe521b6
Ubuntu <ubuntu@ip-172-31-15-155.ap-south-1.compute.internal>
2024-08-18 15:03:36 +0000

    pushing to github

---
+++ Array.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#How to declare an array?
+
+
+Myarray=( 1 2 8 Hello "Hello World!")
+
+
+echo "All the values in array are ${Myarray[*]}"
+echo "The value stored in 3rd index is ${Myarray[3]}"
+
+
+
+
