2b4112f3a45a7bbcaf9e9c4fb4dfb68d0dbe3831
Ubuntu <ubuntu@ip-172-31-15-155.ap-south-1.compute.internal>
2024-08-23 16:51:52 +0000

    pushing code to github

--- Array.sh
+++ Array.sh
@@ -12,4 +12,43 @@ echo "The value stored in 3rd index is ${Myarray[3]}"
 
 
 
+# How to find no. of values in an array?
+
+echo "Number of values, the total length of array ${#Myarray[*]}"
+
+
+
+# How to get any specific index value?
+
+echo "Total index from the range of 2 and 5 ${Myarray[*]:2:3}"
+
+
+#How to update values of Array?
+
+Myarray+=(  New 20 50  )
+
+echo "Values of New Array are ${Myarray[*]}"
+
+
+
+
+#How to store key value pair in an array?
+
+declare -A Myarray
+
+Myarray=( [name]=sai [age]=20 )
+
+echo "My age is ${Myarray[age]}"
+
+echo "The city I live in is ${Myarray[city]}"
+
+ 
+
+
+
+
+
+
+
+
 
