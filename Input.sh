2970f2d8d120d93ac8e428f72264131ee2fd9fd5
Ubuntu <ubuntu@ip-172-31-15-155.ap-south-1.compute.internal>
2024-08-26 11:09:23 +0000

    pushing code to github

---
+++ Input.sh
@@ -0,0 +1,18 @@
+#1/bin/bash
+
+
+#This is a demo script to take input from user.
+
+echo "What is your name?"
+
+read name
+echo "My name is $name."
+
+# Sencond method using -p 
+
+
+read -p "What is your name?" name
+
+echo "My name is $name"
+
+
