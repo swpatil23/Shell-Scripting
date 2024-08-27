05441140646a8aa28d941372d9770138f448bbb0
Ubuntu <ubuntu@ip-172-31-15-155.ap-south-1.compute.internal>
2024-08-27 20:01:12 +0000

    pushing code to github

---
+++ forloop1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+for i in 1 2 3 4 5 6 7 8 9
+do
+	echo "Number is $i"
+done
+
+
+#forloop with string
+
+
+for name in Ram Sham Naam
+do
+	echo "My name is $name"
+done
+
+
+#Range of values demo
+
+
+for i in {1..5}
+do
+	echo "Number is $i"
+done
+
+
+
+
+
+
+
+
+
+
+
+
